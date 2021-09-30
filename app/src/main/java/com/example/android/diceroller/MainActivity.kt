3d5a9711a61f2db2a2a4a891ca6ad46f6c1a48e4
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-09-30 14:13:46 +0200

    Paso 3:Cambiando los numeros con el boton

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,8 +18,9 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +29,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.botone)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "No me pulses, pesao0"+32, Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.resultado)
+        resultText.text = randomInt.toString()
     }
 }
